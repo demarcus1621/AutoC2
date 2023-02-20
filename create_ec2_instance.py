9b22604da859ce11d11897b6bb4f6bd5d3945448
DeMarcus Campbell <demarcus@DeMarcuss-MBP.attlocal.net>
2023-02-19 21:55:04 -0500

    Clean up of some tech debt

--- create_ec2_instance.py
+++ create_ec2_instance.py
@@ -1,15 +1,21 @@
-from pydoc import describe
+import argparse
+import time
 import boto3
 import botocore
-import os
-import time
 import paramiko.client as paramiko
-import argparse
 
-os.environ["AWS_ACCESS_KEY_ID"] = "REMOVED" # EDIT ACCORDINGLY
-os.environ["AWS_SECRET_ACCESS_KEY"] = "REMOVED" # EDIT ACCORDINGLY
 
-def generate_keypair(region:str, keyname="aws_c2"):
+# os.environ["AWS_ACCESS_KEY_ID"] = "REMOVED" # EDIT ACCORDINGLY
+# os.environ["AWS_SECRET_ACCESS_KEY"] = "REMOVED" # EDIT ACCORDINGLY
+
+def generate_keypair(region:str,keyname:str = "aws_c2"):
+    '''
+    Creates a keypair for the AWS account/
+    
+    args:
+    region (str): AWS EC2 region to create a keypair in
+    keyname (str): Name of the key for tracking 
+    '''
     ec2 = boto3.client('ec2', region)
 
     # Checks if there exist any keys within AWS with the same keyname
@@ -26,33 +32,63 @@ def generate_keypair(region:str, keyname="aws_c2"):
         else:
             raise
 
-def delete_keypair(region:str,keyname: str):
+def delete_keypair(region:str,keyname:str):
+    '''
+    Deletes a keypair from the AWS account.
+
+    args:
+    region (str): EC2 region in which the keypair is located
+    keyname (str): Name of the EC2 key to delete
+    '''
     ec2 = boto3.client('ec2', region)
     ec2.delete_key_pair(KeyName=keyname)
     print("Successfully removed keypair")
 
 def get_instance_status(region:str):
+    '''
+    Prints status of running instances within the EC2 region.
+
+    args:
+    region (str): AWS region to query
+    '''
     ec2 = boto3.client('ec2',region)
-    instance_filter = ec2.describe_instances(Filters=[{
-                                            'Name':'instance-state-name',
-                                            'Values':['running']}])['Reservations'][0]['Instances']
+    try:
+        instance_filter = ec2.describe_instances(Filters=[{
+                                                'Name':'instance-state-name',
+                                                'Values':['running']}])['Reservations'][0]['Instances']
+    except:
+        print("Failed to gather instances. Quitting.")
+        return
+    
     print("INSTANCE ID\t\tPUBLIC IP\tPRIVATE IP")
+    
     for instance in instance_filter:
         print(instance['InstanceId'] + "\t"
             + instance['PublicIpAddress'] + "\t"
             + instance['PrivateIpAddress'])
 
-def build_instance( region: str,
+def build_instance( region:str,
                     ami:str,
-                    keyname: str,
-                    group_name: str,
-                    instance_type="t2.micro"):
+                    keyname:str,
+                    group_name:str,
+                    instance_type:str = "t2.micro"):
+    '''
+    Begins the build process for the machine within AWS EC2.
+
+    args:
+    region (str): AWS EC2 region in which to build the machine
+    ami (str): AMI ID for the OS to build the machine with
+    keyname (str): Name of the key to use for building the machine
+    group_name (str): Security group name to configure the machine to have
+    instance_type (str): Machine type to build (ex. T2.micro, t2.large, etc.)
+    '''
     
     ec2 = boto3.client('ec2', region)
 
     # Checks if the security group is created, and if not creates one
     try:
         group = ec2.describe_security_groups(GroupNames=[group_name])
+        print("Group %s already exists. Continuing" % group)
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == 'InvalidGroup.NotFound':
             ec2.create_security_group(GroupName=group_name,
@@ -84,15 +120,31 @@ def build_instance( region: str,
     print("You can now login to the machine with ssh at the following IP address: %s" % instance['Instances'][0]['PublicIpAddress'])
 
 
-def delete_instance(instance_id: str, region:str):
+def delete_instance(instance_id:str,region:str):
+    '''
+    Deletes EC2 instance with given instance ID.
+
+    args:
+    instance_id (str): Instance ID for the EC2 instance to be deleted
+    region (str): AWS region in which the EC2 instance is hosted
+    '''
     ec2 = boto3.client('ec2',region)
     print("Terminating instance")
     ec2.terminate_instances(InstanceIds=[instance_id])
     time.sleep(15)
 
-def configure_instance(c2: str,
-                        ext_ip: str,
-                        key_name: str):
+def configure_instance(c2:str,
+                        ext_ip:str,
+                        key_name:str):
+    '''
+    Configures EC2 instances via Paramiko.
+    Performes a system update, then installs C2 of choice.
+    
+    args:
+    c2 (str): C2 instance of choice [powershell-empire, poshc2]
+    ext_ip (str): The external IP of the EC2 instance
+    key_name (str): Name/Path of the SSH key to use for configuring the instance
+    '''
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
     ssh_client.connect(ext_ip,
@@ -111,8 +163,6 @@ def configure_instance(c2: str,
 
 def main():
     args = parser.parse_args()
-    if args.help:
-        parser.print_help()
 
     if args.createkey:
         generate_keypair(keyname=args.key_name, region=args.REGION)
@@ -124,7 +174,7 @@ def main():
 
     if args.config:
         print("Configuring instance")
-        configure_instance(c2=args.c2,ext_ip=args.ip,)
+        configure_instance(c2=args.c2,ext_ip=args.ip,key_name=args.key_name)
     
 
 if __name__ == '__main__':
